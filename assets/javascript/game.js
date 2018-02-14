e217bcc0589085673a959619dcd0b4910f00df6d
Christopher Etges <Chris@MacBook-Pro.local>
2018-02-14 14:29:43 -0700

    initialWork

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    //all possible words, video game characters
+    var words = ["Mario","Luigi","Donkey Kong","Master Chief", "Link", "Sonic the Hedgehog","Samus Aran", "Solid Snake","Pikachu", "Bowser", "Princess Zelda", "Lara Croft", "Kratos", "Pac-Man","Ryu Hayabusa", "Liu Kang", "Sub-Zero","Duke Nukem", "Nathan Drake",]
+
+
+})
