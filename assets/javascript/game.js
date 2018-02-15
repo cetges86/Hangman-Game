a4de864c4a73e7932b5b5ae0e05790b291bef5d1
Christopher Etges <Chris@MacBook-Pro.local>
2018-02-14 23:33:25 -0700

    workWed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //all possible words, video game characters
-    var words = ["Mario","Luigi","Donkey Kong","Master Chief", "Link", "Sonic the Hedgehog","Samus Aran", "Solid Snake","Pikachu", "Bowser", "Princess Zelda", "Lara Croft", "Kratos", "Pac-Man","Ryu Hayabusa", "Liu Kang", "Sub-Zero","Duke Nukem", "Nathan Drake",]
+    var words = ["Mario", "Luigi", "DonkeyKong", "MasterChief", "Link", "SonictheHedgehog", "SamusAran", "SolidSnake", "Pikachu", "Bowser", "PrincessZelda", "LaraCroft", "Kratos", "PacMan", "Scorpion", "LiuKang", "SubZero", "DukeNukem", "NathanDrake"];
 
 
+
+    var randomWord = words[Math.floor(Math.random() * words.length)];
+    var spaces = randomWord.length;
+    console.log(randomWord);
+    console.log(spaces);
+
+    for (i = 0; i < randomWord.length; i++) {
+        $('#currentWord').append("<li>" + randomWord.charAt(i).toUpperCase() + "</li>");
+
+    };
+
+    guessCount = 10;
+    $('#guesses').append(" " + guessCount);
+    for (i = 10; i > 0; i--) {
+
+    }
 })
