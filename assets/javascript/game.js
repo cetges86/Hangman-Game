bb2ccfe2e5647ac3fa8cc6a17520f88bc4b683b7
Christopher Etges <Chris@MacBook-Pro.local>
2018-02-16 21:37:40 -0700

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,20 +10,66 @@ $(document).ready(function () {
     console.log(spaces);
 
     for (i = 0; i < randomWord.length; i++) {
-        $('#currentWord').append("<li id=" + i + ">" + randomWord.charAt(i).toUpperCase() + "</li>");
+        var letter = randomWord.charAt(i).toUpperCase();
+        $('#currentWord').append("<li id=" + letter + ">" + letter + "</li>");
 
     };
 
     guessCount = 10;
     $('#guesses').append(" " + guessCount);
 
-    document.onkeyup = function(event){
-        var currentGuess = event.key;   
-        console.log(currentGuess);  
-        console.log(randomWord.toLowerCase().indexOf(currentGuess)); 
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var lettersSolved = 0;
+    var lettersGuessed = [];
 
+    document.onkeyup = function (event) {
+        var currentGuess = event.key;
+        var validLetter = alphabet.indexOf(currentGuess);
+        console.log(currentGuess);
+        var position = randomWord.toLowerCase().indexOf(currentGuess);
+
+
+
+        if (position === -1 &&
+            lettersSolved != randomWord.length &&
+            !lettersGuessed.includes(currentGuess) &&
+            validLetter != -1) {
+
+            guessCount--;
+            alert("Try again!");
+            lettersGuessed.push(currentGuess);
+            $('#letters').html("Letters Guessed:<br> " + lettersGuessed);
+            $('#guesses').html("Guesses Remaining: " + guessCount);
+
+        } else if (position > -1 &&
+            lettersSolved != randomWord.length &&
+            !lettersGuessed.includes(currentGuess) &&
+            validLetter != -1) {
+
+            $('#' + currentGuess.toUpperCase() + '').addClass("revealed");
+            lettersGuessed.push(currentGuess);
+            $('#letters').html("Letters Guessed: <br>" + lettersGuessed);
+            lettersSolved++;
+
+            if (lettersSolved === randomWord.length) {
+                alert("You win!!");
+            };
+
+            // for (var j = 0; j < randomWord.length; j++) {
+            //     if (randomWord.charAt(j) === currentGuess) {
+            //         lettersSolved++;
+            //         console.log(lettersSolved);
+            //         $('#' + currentGuess.toUpperCase() + '').addClass("revealed");
+            //     };
+            // };
+
+    } else if (lettersGuessed.includes(currentGuess)) {
+        alert("Letter has already been guessed!");
+    } else {
+        alert("That key isn't even a letter!")
     };
 
 
-        
+
+
 });
